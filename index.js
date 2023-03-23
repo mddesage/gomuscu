af748dd6c2fac9b58e825e049ff3efda171b3100
mddesage <mddesage@gmail.com>
2023-03-23 15:53:19 +0100

    reinvit

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ client.on("messageCreate", async message => {
                         .setFooter("Au nom de l'équipe  𝐺𝑂𝑀𝑈𝑆𝐶𝑈.")
                         .setImage("https://images-ext-2.discordapp.net/external/gXakbSDik9kWaj6hawV9rAI9bXb0G0IpVspJhvL96xw/https/www.zupimages.net/up/22/27/smao.png?width=1440&height=399")
                         .setThumbnail("https://cdn.discordapp.com/attachments/987820203016618015/1088231600854143077/gars_et_fille_body.png")
-                        .setDescription("Le prefix de <@994859660727291985> est **sa mention**.\n\n<@994859660727291985> **suppr**\n<@994859660727291985> **code**\n<@994859660727291985> **repete**\n<@994859660727291985> **repete&suppr**")
+                        .setDescription("Le prefix de <@994859660727291985> est **sa mention**.\n\n<@994859660727291985> **suppr**\n<@994859660727291985> **code**\n<@994859660727291985> **repete**\n<@994859660727291985> **repete&suppr**\n<@994859660727291985> **reinvite [user_id]**")
                         .setTitle("Liste des commandes EMPLOYÉS");
                     message.channel.send({ embeds: [embed] });
                     } else {
