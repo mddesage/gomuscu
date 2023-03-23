44a0045e62e160d74d89fefe1fb5c16b72946e3c
mddesage <mddesage@gmail.com>
2023-03-23 22:50:16 +0100

    release

--- index.js
+++ index.js
@@ -577,12 +577,19 @@ client.on('messageCreate', async message => {
         .setStyle('PRIMARY');
     });
 
+    // Créer le bouton Aléatoire
+    const boutonAleatoire = new MessageButton()
+      .setCustomId('groupeMusculaire-aleatoire')
+      .setLabel('Aléatoire')
+      .setStyle('PRIMARY');
+
     // Diviser les boutons en deux lignes
     const row1 = new MessageActionRow().addComponents(boutons.slice(0, 5));
     const row2 = new MessageActionRow().addComponents(boutons.slice(5, 10));
+    const row3 = new MessageActionRow().addComponents(boutonAleatoire);
 
     // Envoyer le message avec les boutons
-    await message.channel.send({ content: 'Choisissez un groupe musculaire pour afficher un exercice :', components: [row1, row2] });
+    await message.channel.send({ content: 'Choisissez un groupe musculaire pour afficher un exercice au hasard parmi celui si :', components: [row1, row2, row3] });
   }
 });
 
@@ -590,10 +597,17 @@ client.on('interactionCreate', async interaction => {
   if (!interaction.isButton()) return;
 
   const buttonId = interaction.customId;
-  const index = parseInt(buttonId.split('-')[1]);
+  const index = buttonId.split('-')[1];
 
   const groupesMusculaires = ['épaules', 'biceps', 'triceps', 'pectoraux', 'abdominaux', 'dos', 'fessiers', 'ischios jambiers', 'quadriceps', 'mollets'];
-  const groupeMusculaire = groupesMusculaires[index];
+  let groupeMusculaire;
+
+  if (index === 'aleatoire') {
+    const randomIndex = Math.floor(Math.random() * groupesMusculaires.length);
+    groupeMusculaire = groupesMusculaires[randomIndex];
+  } else {
+    groupeMusculaire = groupesMusculaires[parseInt(index)];
+  }
 
   let exercicesFiltres = exercices.filter(e => e.groupeMusculaire === groupeMusculaire);
 
