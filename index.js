99a51a9fe7bd9b289f39209224d1cd2af23b6f04
mddesage <mddesage@gmail.com>
2023-03-23 19:51:21 +0100

    exercicehelp

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ client.on("messageCreate", async message => {
             case 'help':
                     const embed = new Discord.MessageEmbed()
                         .setColor("BLUE")
-                        .setFooter("Au nom de l'équipe 𝐺𝑂𝑀𝑈𝑆𝐶𝑈.")
+                        .setFooter({ text: "Au nom de l'équipe 𝐺𝑂𝑀𝑈𝑆𝐶𝑈." })
                         .setImage("https://images-ext-2.discordapp.net/external/gXakbSDik9kWaj6hawV9rAI9bXb0G0IpVspJhvL96xw/https/www.zupimages.net/up/22/27/smao.png?width=1440&height=399")
                         .setThumbnail("https://cdn.discordapp.com/attachments/987820203016618015/1088231600854143077/gars_et_fille_body.png")
                         .setDescription("Le prefix de <@994859660727291985> est **sa mention**.\n\n<@994859660727291985> **discord** \n<@994859660727291985> **roles**\n<@994859660727291985> **departements**\n<@994859660727291985> **discussion**\n<@994859660727291985> **performances**\n<@994859660727291985> **evolutions**\n<@994859660727291985> **programmes**\n<@994859660727291985> **playlist**\n<@994859660727291985> **reseauxsociaux**\n<@994859660727291985> **gymbro**")
@@ -152,7 +152,7 @@ client.on("messageCreate", async message => {
                 if (message.member.permissions.has("ADMINISTRATOR")) {
                     const embed = new Discord.MessageEmbed()
                         .setColor("RED")
-                        .setFooter("Au nom de l'équipe  𝐺𝑂𝑀𝑈𝑆𝐶𝑈.")
+                        .setFooter({ text: "Au nom de l'équipe 𝐺𝑂𝑀𝑈𝑆𝐶𝑈." })
                         .setImage("https://images-ext-2.discordapp.net/external/gXakbSDik9kWaj6hawV9rAI9bXb0G0IpVspJhvL96xw/https/www.zupimages.net/up/22/27/smao.png?width=1440&height=399")
                         .setThumbnail("https://cdn.discordapp.com/attachments/987820203016618015/1088231600854143077/gars_et_fille_body.png")
                         .setDescription("Le prefix de <@994859660727291985> est **sa mention**.\n\n<@994859660727291985> **suppr** *[nombre de messages]*\n<@994859660727291985> **code**\n<@994859660727291985> **repete** *[message]*\n<@994859660727291985> **repete&suppr** *[message]*\n<@994859660727291985> **reinvite** *[user_id]*")
@@ -473,7 +473,7 @@ const { exercices } = require('./commands/exercices.js');
             message.content.toLowerCase().startsWith(prefix + 'exercices help')) {
           const embed = new Discord.MessageEmbed()
             .setColor("GREEN")
-            .setFooter("Au nom de l'équipe 𝐺𝑂𝑀𝑈𝑆𝐶𝑈.")
+            .setFooter({ text: "Au nom de l'équipe 𝐺𝑂𝑀𝑈𝑆𝐶𝑈." })
             .setImage("https://images-ext-2.discordapp.net/external/gXakbSDik9kWaj6hawV9rAI9bXb0G0IpVspJhvL96xw/https/www.zupimages.net/up/22/27/smao.png?width=1440&height=399")
             .setThumbnail("https://cdn.discordapp.com/attachments/987820203016618015/1088231600854143077/gars_et_fille_body.png")
             .setDescription("s")
@@ -500,12 +500,12 @@ client.on("messageCreate", async message => {
         case 'helpexercices':
           const embed = new Discord.MessageEmbed()
             .setColor("GREEN")
-            .setFooter("Au nom de l'équipe 𝐺𝑂𝑀𝑈𝑆𝐶𝑈.")
+            .setFooter({ text: "Au nom de l'équipe 𝐺𝑂𝑀𝑈𝑆𝐶𝑈." })
             .setImage("https://images-ext-2.discordapp.net/external/gXakbSDik9kWaj6hawV9rAI9bXb0G0IpVspJhvL96xw/https/www.zupimages.net/up/22/27/smao.png?width=1440&height=399")
             .setThumbnail("https://cdn.discordapp.com/attachments/987820203016618015/1088231600854143077/gars_et_fille_body.png")
             .setDescription("Le prefix de <@994859660727291985> est **sa mention**.\n\n\n<@994859660727291985> **exercice** \n Envoie un exercice au hasard sur n'importe quel groupe musculaire (75 possiblité)\n\n\n<@994859660727291985> **exercice** *[groupe musculaire]*\nEnvoie un exercice au hasard sur le groupe musculaire cité (11 groupes musculaire)\n       - épaules\n       - biceps\n       - triceps\n       - avants bras\n       - pectoraux\n       - abdominaux\n       - dos\n       - fessiers\n       - ischios jambiers\n       - quadriceps\n       - mollets")
             .setTitle("Liste des commandes EXERCICE");
-          message.channel.send({ embeds: [embed] });
+            message.channel.send({ embeds: [embed] });
           break;
       }
     }
