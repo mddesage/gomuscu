8ee95eaddc17587baee42f25a58876a1a3ec13eb
mddesage <mddesage@gmail.com>
2023-03-23 19:59:31 +0100

    exercicehelp

--- index.js
+++ index.js
@@ -482,3 +482,15 @@ const { exercices } = require('./commands/exercices.js');
           message.channel.send(embed);
         }
       });
+  client.on('message', message => {
+    if (message.content === prefix + "exercicehelp") {
+        const embed = new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setFooter({ text: "Au nom de l'équipe 𝐺𝑂𝑀𝑈𝑆𝐶𝑈." })
+        .setImage("https://images-ext-2.discordapp.net/external/gXakbSDik9kWaj6hawV9rAI9bXb0G0IpVspJhvL96xw/https/www.zupimages.net/up/22/27/smao.png?width=1440&height=399")
+        .setThumbnail("https://cdn.discordapp.com/attachments/987820203016618015/1088231600854143077/gars_et_fille_body.png")
+        .setDescription("Le prefix de <@994859660727291985> est **sa mention**.\n\n\n<@994859660727291985> **exercice** \n Envoie un exercice au hasard sur n'importe quel groupe musculaire (75 possiblité)\n\n\n<@994859660727291985> **exercice** *[groupe musculaire]*\nEnvoie un exercice au hasard sur le groupe musculaire cité (11 groupes musculaire)\n       - épaules\n       - biceps\n       - triceps\n       - avants bras\n       - pectoraux\n       - abdominaux\n       - dos\n       - fessiers\n       - ischios jambiers\n       - quadriceps\n       - mollets")
+        .setTitle("Liste des commandes EXERCICE");
+        message.channel.send({ embeds: [embed] });
+    }
+  });
