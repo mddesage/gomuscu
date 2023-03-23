5823379be85ce153c9893674094af3303bcce42c
mddesage <mddesage@gmail.com>
2023-03-23 19:30:02 +0100

    exercice

--- index.js
+++ index.js
@@ -471,7 +471,7 @@ const { exercices } = require('./commands/exercices.js');
             message.content.toLowerCase().startsWith(prefix + 'exercices aide') ||
             message.content.toLowerCase().startsWith(prefix + 'exercices help')) {
           const embed = new Discord.MessageEmbed()
-            .setColor("GREEN")
+            .setColor("BLUE")
             .setFooter("Au nom de l'équipe 𝐺𝑂𝑀𝑈𝑆𝐶𝑈.")
             .setImage("https://images-ext-2.discordapp.net/external/gXakbSDik9kWaj6hawV9rAI9bXb0G0IpVspJhvL96xw/https/www.zupimages.net/up/22/27/smao.png?width=1440&height=399")
             .setThumbnail("https://cdn.discordapp.com/attachments/987820203016618015/1088231600854143077/gars_et_fille_body.png")
