bfd2d0da41a974fe8f3591de8e9dbf72df958298
mddesage <mddesage@gmail.com>
2023-03-26 20:22:49 +0200

    release

--- commands/suppr.js
+++ commands/suppr.js
@@ -8,19 +8,16 @@ module.exports = {
   permissions: [Permissions.FLAGS.ADMINISTRATOR],
 
   async execute(message, args) {
-    // Vérifie si l'utilisateur a la permission de supprimer les messages
     if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
       return message.reply('Vous n\'avez pas la permission de supprimer des messages.');
     }
 
-    // Vérifie si l'argument est un nombre
     const amount = parseInt(args[0]);
 
     if (isNaN(amount)) {
       return message.reply('L\'argument doit être un nombre.');
     }
 
-    // Supprime les x derniers messages
     if (amount <= 0 || amount > 100) {
       return message.reply('Le nombre de messages doit être compris entre 1 et 100.');
     }
