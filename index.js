6395df9ab0d3036f52c5a330ab09c1a3530c94be
mddesage <mddesage@gmail.com>
2023-03-26 01:43:14 +0100

    release

--- index.js
+++ index.js
@@ -1302,5 +1302,21 @@ function play(guild, song) {
   player.on('error', (error) => {
     console.error(error);
     serverQueue.textChannel.send('Il y a eu une erreur lors de la lecture de la vidéo.');
+    
   });
 }
+
+client.on('messageCreate', async (message) => {
+
+  if (command === 'musiqueaide') {
+    const embed = new MessageEmbed()
+      .setColor("BLUE")
+      .setFooter({ text: "Au nom de l'équipe 𝐺𝑂𝑀𝑈𝑆𝐶𝑈." })
+      .setImage("https://images-ext-2.discordapp.net/external/gXakbSDik9kWaj6hawV9rAI9bXb0G0IpVspJhvL96xw/https/www.zupimages.net/up/22/27/smao.png?width=1440&height=399")
+      .setThumbnail("https://cdn.discordapp.com/attachments/987820203016618015/1088231600854143077/gars_et_fille_body.png")
+      .setDescription("\nLe prefix de <@994859660727291985> est **sa mention**.\n\n<@994859660727291985> *[le salon que vous cherchez]*\n\n<@994859660727291985> **exerciceaide**\n<@994859660727291985> **exercice**\n<@994859660727291985> **exercice+**\n\n<@994859660727291985> **chatgpt**\n\n<@994859660727291985> **musique** *[lien YouTube]*")
+      .setTitle("Liste des commandes GLOBALES");
+
+    message.channel.send({ embeds: [embed] });
+  }
+});
