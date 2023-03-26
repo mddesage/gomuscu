10299347253af3132a0c1f89a70ca77971cac2fa
mddesage <mddesage@gmail.com>
2023-03-26 22:03:14 +0200

    release

--- index.js
+++ index.js
@@ -614,8 +614,8 @@ client.on("messageCreate", async message => {
         case 'helpexercices':
           const embed = new Discord.MessageEmbed()
             .setColor("YELLOW")
-            .setFooter({ text: "Au nom de l'équipe 𝐺𝑂𝑀𝑈𝑆𝐶𝑈."; 
-                         image: "https://cdn.discordapp.com/attachments/987820203016618015/1088231600854143077/gars_et_fille_body.png"})
+            .setFooter({ text: "Au nom de l'équipe 𝐺𝑂𝑀𝑈𝑆𝐶𝑈.", 
+                         image: { url: "https://cdn.discordapp.com/attachments/987820203016618015/1088231600854143077/gars_et_fille_body.png"}})
             .setImage("https://images-ext-2.discordapp.net/external/gXakbSDik9kWaj6hawV9rAI9bXb0G0IpVspJhvL96xw/https/www.zupimages.net/up/22/27/smao.png?width=1440&height=399")
             .setThumbnail("https://cdn.discordapp.com/attachments/987820203016618015/1088231600854143077/gars_et_fille_body.png")
             .setDescription(`
