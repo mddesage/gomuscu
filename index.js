eebff16004c0a0c09d95c26faa9c4207bef03a38
mddesage <mddesage@gmail.com>
2023-04-20 20:51:09 +0200

    release

--- index.js
+++ index.js
@@ -2348,7 +2348,7 @@ client.on('messageCreate', message => {
 <@&987826511295950889> & <@&987826660130816020>              **Alphalete**    →     *https://alphaleteathletics.com/*
 <@&987826511295950889> & <@&987826660130816020>              **Bulk**              →     *https://www.bulk.com/fr/*
 <@&987826511295950889>                                                **Fabletics**      →       *https://www.fabletics.fr/*
-<@&987826511295950889> & <@&987826660130816020>              **Gymshark**    →     **https://fr.gymshark.com/*
+<@&987826511295950889> & <@&987826660130816020>              **Gymshark**    →     *https://fr.gymshark.com/*
 <@&987826511295950889>                                                **Lululemon**   →       *https://www.lululemon.fr/*
 <@&987826511295950889> & <@&987826660130816020>              **My Protein**  →     *https://fr.myprotein.com/*
 <@&987826511295950889> & <@&987826660130816020>              **Prozis**            →     *https://www.prozis.com/fr/*
