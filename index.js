b5dfc27b56f7229005dfa603b9a57f64091c181a
mddesage <mddesage@gmail.com>
2023-04-29 17:41:18 +0200

    release

--- index.js
+++ index.js
@@ -2105,28 +2105,26 @@ client.on('interactionCreate', async interaction => {
 
 client.on('messageCreate', async message => {
   if (!message.content.startsWith(prefix)) return;
-
+  
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
-
+  
   if (command === 'quirole') {
-    if (!message.member.roles.cache.has(requiredEmployedRoleId)) {
-      return message.reply("Désolé, cette commande est réservée aux employés.");
-    }
-
-    if (!args[0]) return message.reply('Veuillez mentionner un rôle.');
-    const role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0].replace(/[^0-9]/g, ''));
-    if (!role) return message.reply('Rôle non valide.');
-
-    const membersWithRole = role.members.map(member => member.toString()).join('\n');
-    const embed = new MessageEmbed()
-      .setColor('GREEN')
-      .setTitle(`Membres avec le rôle ${role.name}`)
-      .setDescription(membersWithRole);
-
-    await message.channel.send({ embeds: [embed] });
-  }
-})
+  if (!message.member.roles.cache.has(requiredEmployedRoleId)) {
+  return message.reply("Désolé, cette commande est réservée aux employés.");
+  }if (!args[0]) return message.reply('Veuillez mentionner un rôle.');
+  const role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0].replace(/[^0-9]/g, ''));
+  if (!role) return message.reply('Rôle non valide.');
+  
+  const membersWithRole = role.members.map(member => member.toString()).join('\n');
+  const embed = new MessageEmbed()
+    .setColor('GREEN')
+    .setTitle(`Membres avec le rôle ${role.name} (${role.members.size} membres)`)
+    .setDescription(membersWithRole);
+  
+  await message.channel.send({ embeds: [embed] });
+}
+})  
 
 
 client.on('messageCreate', async message => {
